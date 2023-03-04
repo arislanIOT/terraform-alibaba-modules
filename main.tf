5bdb1c9c621ea32aec50aa546993a9eb0bc6d2ba
arislanIOT <riya@Riyas-MacBook-Air.local>
2023-03-04 11:49:46 +0530

    vswitches

--- main.tf
+++ main.tf
@@ -8,16 +8,37 @@
 # }
 
 provider "alicloud" {
+
   region = "${var.region}"
   profile = "${var.profile}"
+
 }
 
 module "vpc" {
     source = "./module/vpc"
     vpc_name = "myvpc"
     cidr_block = "172.160.0.0/12"
-    # resource_group_id = "myvpc-group"
+    vpc_tags = {
+      
+      Owner       = "user"
+      Environment = "dev"
+      Name        = "complete"
+    }
     
-    
-  
+}
+
+module "vswitches" {
+
+  source = "./module/vswitches"
+  vswitch_name = "myvswitch"
+  vpc_id = module.vpc.vpc_id
+  cidr_block = ["172.160.1.0/24", "172.160.2.0/24"]
+  zone_id = ["${var.region}", "${var.region}"]
+
+  vswitch_tags = {
+
+    "Owner" = "user",
+    "Environment" = "dev"
+  }
+
 }
